0c78041e6c6209853f29d719ad6526d085684c57
Nick Garza <ngarza@cainc.com>
2019-08-01 10:43:52 -0400

    paragraph class challenge

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,12 +1,26 @@
-const link = document.querySelector('a');
+// const link = document.querySelector('a');
 
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.npr.org/');
-link.innerText = "This is EN-PEE-AH";
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.npr.org/');
+// link.innerText = "This is EN-PEE-AH";
 
-const para = document.querySelector('p');
-para.setAttribute('class', 'success');
-para
+// const para = document.querySelector('p');
+// para.setAttribute('class', 'success');
+// para
 
-const title = document.querySelector('h1');
-title.style.margin = '50px';
+// const title = document.querySelector('h1');
+// title.style.margin = '50px';
+
+// const content = document.querySelector('p');
+// content.classList.add('test');
+// content.classList.remove('error');
+// content.classList.add('success');
+// console.log(content.classList);
+
+const paraList = document.querySelectorAll('p');
+console.log(paraList);
+paraList.forEach(para => {
+    console.log(para.innerHTML);
+    para.innerHTML.includes('error') && para.classList.add('error');
+    para.innerHTML.includes('success') && para.classList.add('success');
+})
