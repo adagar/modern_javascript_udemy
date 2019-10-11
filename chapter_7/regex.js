aecab6a09f8ae79ab3aa8d1f49d15f59a0630053
Nick Garza <adagar121987@gmail.com>
2019-10-11 16:39:34 -0400

    Regex sandbox

---
+++ chapter_7/regex.js
@@ -0,0 +1,17 @@
+// https://regex101.com/
+
+// certain character (single)
+/^[a-z]$
+
+// certain character, any case
+/^[a-zA-Z]$
+
+// word of certain length
+/^[a-zA-Z]{6,10}$
+
+// numbers
+/^[0-9]{6-10}$
+
+
+// any character, with numbers
+/^.[0-9]{6-10}$
