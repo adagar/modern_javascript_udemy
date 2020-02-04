c8de1a8ca3a9805dc4710962112d4801875b2b02
Nick Garza <adagar121987@gmail.com>
2020-02-04 16:28:43 -0500

    Live form validation

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,9 +1,30 @@
 const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
 
-form.addEventListener('submit' evt => {
+form.addEventListener('submit', evt => {
     evt.preventDefault();
 
+    testUserName();
+});
+
+
+// live feedback
+form.username.addEventListener('keyup', evt => {
+    evt.preventDefault();
+
+    testUserName();
+})
+
+const testUserName = () => {
     //validation
     const username = form.username.value;
     const usernamePattern = /^[a-zA-Z]{6,12}$/; //any alpha char, any case, 6-12 characters long
-})
+
+    if(usernamePattern.test(username)) {
+        //feedback good info
+        feedback.textContent = 'That username is valid!';
+    } else {
+        // feedback bad info
+        feedback.textContent = "Username must only containers letters and numbers, without 6-12 characters";
+    }
+}
